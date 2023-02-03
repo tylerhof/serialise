3d59ba422320f1eb806fde43f2aaf12a23e9f461
tylerhof <tyler.hofmeister@gmail.com>
2023-02-03 12:51:39 +0000

    make WriteDictsToCsvs more customizable, using a configured mode

--- serialise/save.py
+++ serialise/save.py
@@ -5,13 +5,14 @@ from exceptionhandling.functor import Functor
 
 class SaveDictsAsCsv(Functor):
 
-    def __init__(self, file, policy: ExceptionHandler = Safe()):
+    def __init__(self, file, mode = 'w', policy: ExceptionHandler = Safe()):
         super().__init__(policy)
         self.file = file
+        self.mode = mode
 
     def apply(self, input):
         if len(input) > 0:
-            with open(self.file, 'w') as open_file:
+            with open(self.file, self.mode) as open_file:
                 w = csv.DictWriter(open_file, input[0].keys())
                 for dict in input:
                     w.writerows(dict.items())
